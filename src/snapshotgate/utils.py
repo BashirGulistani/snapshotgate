c3e85ca4eb99aa0dd6ef8e3d0142da1459b9c315
Bashir Gulistani <76609477+BashirGulistani@users.noreply.github.com>
2026-02-15 22:25:59 -0500

    Update utils.py

--- src/snapshotgate/utils.py
+++ src/snapshotgate/utils.py
@@ -9,4 +9,12 @@ from typing import Any
 _ws = re.compile(r"\s+")
 
 
+def norm_str(x: Any) -> str:
+    if x is None:
+        return ""
+    s = str(x).strip()
+    s = _ws.sub(" ", s)
+    return s
+
+
 
