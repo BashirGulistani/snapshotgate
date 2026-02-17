fb21f2cc3493616f7793dbefb0178da9ad28d67c
Bashir Gulistani <76609477+BashirGulistani@users.noreply.github.com>
2026-02-16 22:08:17 -0500

    Update profiler.py

--- src/snapshotgate/profiler.py
+++ src/snapshotgate/profiler.py
@@ -20,3 +20,32 @@ class ColProfile:
     num_mean: float
     num_m2: float  
 
+    def add(self, v: Any) -> None:
+        self.count += 1
+        if is_empty(v):
+            self.nulls += 1
+            return
+
+        sv = norm_str(v)
+        fb = try_bool(v)
+        fd = try_dateish(v)
+        ff = try_float(v)
+
+        if ff is not None:
+            self.num_count += 1
+            delta = ff - self.num_mean
+            self.num_mean += delta / self.num_count
+            delta2 = ff - self.num_mean
+            self.num_m2 += delta * delta2
+        elif fb is not None:
+            pass
+        elif fd is not None:
+            pass
+        else:
+            pass
+
+
+
+
+
+
